daf9266a5fa4122ba5b08ac11b752eb0a65812b7
souravKumarBGP <souravkumarbgp1010@gmail.com>
2024-01-06 21:45:47 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,6 @@
+/******************************************** Start loading logic **************************************** */
+let loaderBox = document.querySelector(".loading");
+window.addEventListener("load",() =>{
+    loaderBox.style.display = "none";
+});
+/****************************************** End loading logic ******************************************** */
