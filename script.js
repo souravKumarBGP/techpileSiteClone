1886bf824e2ea2b3f2b726e0cb6824e1a85272df
souravKumarBGP <souravkumarbgp1010@gmail.com>
2024-01-08 09:58:36 +0530

    motify in script and js file

--- script.js
+++ script.js
@@ -4,3 +4,11 @@ window.addEventListener("load",() =>{
     loaderBox.style.display = "none";
 });
 /****************************************** End loading logic ******************************************** */
+
+
+let navLinks = document.querySelectorAll(".nav-link");
+for(items of navLinks){
+    items.addEventListener("click", () =>{
+        items.classList.add = "active";
+    })
+}
