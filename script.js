9014b759391ae0a6213c26895e071e6baf687e45
souravKumarBGP <souravkumarbgp1010@gmail.com>
2024-02-07 10:50:35 +0530

    commit 07

--- script.js
+++ script.js
@@ -4,11 +4,21 @@ window.addEventListener("load",() =>{
     loaderBox.style.display = "none";
 });
 /****************************************** End loading logic ******************************************** */
+let headerManue = document.querySelector(".headerManue");
+window.addEventListener("scroll", () =>{
+    if(window.scrollY >= "25"){
+        headerManue.style.position = "fixed";
+        headerManue.style.top = "0px";
+        headerManue.style.left = "0px";
+    }else{
+        headerManue.style.position = "static";
 
+    }
+})
 
-let navLinks = document.querySelectorAll(".nav-link");
-for(items of navLinks){
-    items.addEventListener("click", () =>{
-        items.classList.add = "active";
-    })
-}
+// let navLinks = document.querySelectorAll(".nav-link");
+// for(items of navLinks){
+//     items.addEventListener("click", () =>{
+//         items.classList.add = "active";
+//     })
+// }
